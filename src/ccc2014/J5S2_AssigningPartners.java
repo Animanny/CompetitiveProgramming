9732e60cf273633a841b4963469dd637d8b500ee
Ani Srikanth <ani.srikanth23@gmail.com>
2018-01-22 17:19:35 -0500

    HWY NOT WORKING

--- src/ccc2014/J5S2_AssigningPartners.java
+++ src/ccc2014/J5S2_AssigningPartners.java
@@ -1,6 +1,7 @@
 package ccc2014;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class J5S2_AssigningPartners {
@@ -8,7 +9,7 @@ public class J5S2_AssigningPartners {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		state currentstate = state.GOOD;
+		boolean currentState = true;
 		int n = sc.nextInt();
 
 		ArrayList<String> names1 = new ArrayList<String>();
@@ -22,14 +23,18 @@ public class J5S2_AssigningPartners {
 		}
 
 		for (int i = 0; i < n; i++) {
-			if (names1.get(i).equals((names2.get((n - 1 - i))))) {
-
+			String currentPartner = names2.get(i);
+			String currentPer = names1.get(i);
+			
+			
+			if (currentPartner.equals(names1.get(names2.indexOf(currentPer)))){
+				
 			} else {
-				currentstate = state.BAD;
+				currentState = false;
 			}
 		}
 
-		if (currentstate == state.GOOD) {
+		if (currentState) {
 			System.out.println("good");
 		} else {
 			System.out.println("bad");
@@ -37,8 +42,4 @@ public class J5S2_AssigningPartners {
 
 	}
 
-}
-
-enum state {
-	GOOD, BAD;
 }
